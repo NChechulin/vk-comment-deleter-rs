91b023052fe99358554ad885f6e3b500ddf73cdc
Nikolay Chechulin <nchechulin@aol.com>
2021-05-14 17:10:57 +0300

    add simple function for validating the token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate regex;
+use dialoguer::Input;
+use regex::Regex;
+
+
+/// Checks whether the token consists of a required set of characters
+fn token_first_validation(token: &String) -> bool {
+    let re = Regex::new("^[a-z0-9]{85}$").unwrap();
+    re.is_match(token)
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world!");
 }
