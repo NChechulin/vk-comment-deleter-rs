d7371c4608a4fd36e35037c25187eecd90718f86
Nikolay Chechulin <nchechulin@aol.com>
2021-05-14 19:22:32 +0300

    add function for getting file contents

--- src/comment_reading.rs
+++ src/comment_reading.rs
@@ -1,5 +1,24 @@
+use encoding_rs::WINDOWS_1251;
+use encoding_rs_io::DecodeReaderBytesBuilder;
+use regex::Regex;
+use std::fs::File;
+use std::io::{BufReader, Read};
+
 pub struct Comment {
-    owner_id: String,
-    comment_id: String,
+    pub owner_id: String,
+    pub comment_id: String,
 }
 
+fn read_file_contents(path: &String) -> String {
+    let file = File::open(path).expect("Unable to open the file");
+    let mut reader = BufReader::new(
+        DecodeReaderBytesBuilder::new()
+            .encoding(Some(WINDOWS_1251))
+            .build(file),
+    );
+
+    let mut result = String::new();
+    reader.read_to_string(&mut result).unwrap();
+
+    result
+}
