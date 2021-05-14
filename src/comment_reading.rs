6e4d8c2cf503d9a91f2e7f3be496ca6d58b0d18f
Nikolay Chechulin <nchechulin@aol.com>
2021-05-14 20:31:44 +0300

    add a method for getting a vector of files with comments

--- src/comment_reading.rs
+++ src/comment_reading.rs
@@ -1,12 +1,28 @@
 use encoding_rs::WINDOWS_1251;
 use encoding_rs_io::DecodeReaderBytesBuilder;
 use regex::Regex;
-use std::fs::File;
+use std::fs::{read_dir, File};
 use std::io::{BufReader, Read};
 
 pub struct Comment {
     pub owner_id: String,
     pub comment_id: String,
+}
+
+pub fn get_list_of_files(folder_path: &String) -> Vec<String> {
+    let mut result: Vec<String> = vec![];
+    let paths = read_dir(folder_path).unwrap();
+    let re = Regex::new(r"comments(\d{0,10}).html").unwrap(); // Someone teach me regex...
+
+    for path in paths {
+        let path_str = path.unwrap().path().display().to_string();
+        // if current file has form of `comments.html` or `comments<any_number_here>.html`
+        if re.is_match(&path_str) {
+            result.push(path_str);
+        }
+    }
+
+    result
 }
 
 fn read_file_contents(path: &String) -> String {
@@ -23,7 +39,6 @@ fn read_file_contents(path: &String) -> String {
     result
 }
 
-
 fn get_comments_from_file(path: &String) -> Vec<Comment> {
     let re = Regex::new(r"https://vk.com/wall(-?\d+)_\d+\?reply=(\d+)").unwrap();
     let mut result = Vec::new();
@@ -35,6 +50,17 @@ fn get_comments_from_file(path: &String) -> Vec<Comment> {
             owner_id: capture[1].to_string(),
             comment_id: capture[2].to_string(),
         });
+    }
+
+    result
+}
+
+pub fn get_all_comments(folder_path: &String) -> Vec<Comment> {
+    let mut result = vec![];
+
+    for file in get_list_of_files(folder_path) {
+        let mut comments_from_file = get_comments_from_file(&file);
+        result.append(&mut comments_from_file);
     }
 
     result
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ pub mod cli;
 pub mod comment_reading;
 
 fn main() {
-    let path = String::from("/home/nikolay/Downloads/comments/comments.html");
-    let result = comment_reading::get_comments_from_file(&path);
+    let path = String::from("/home/nikolay/Downloads/comments");
+    // let result = comment_reading::get_list_of_files(&path);
+    // for path in result {
+    //     println!("{}", path);
+    // }
+    let result = comment_reading::get_all_comments(&path);
     println!("{}", result.len());
+    // let result = comment_reading::get_comments_from_file(&path);
+    // println!("{}", result.len());
     // cli::read_token();
     // println!("{}", cli::read_agreement());
 }
