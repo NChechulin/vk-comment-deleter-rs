d70577dc0e059cb7ee204484a88a269b850bcaa0
Nikolay Chechulin <nchechulin@aol.com>
2021-05-14 17:43:08 +0300

    add function for reading token, modify the return type of token_first_validation

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,30 @@ extern crate regex;
 use dialoguer::Input;
 use regex::Regex;
 
-
-/// Checks whether the token consists of a required set of characters
-fn token_first_validation(token: &String) -> bool {
+/// Проверяет, может ли введенная строка быть токеном (но не дает 100%-й гарантии)
+fn token_first_validation(token: &String) -> Result<(), &'static str> {
     let re = Regex::new("^[a-z0-9]{85}$").unwrap();
-    re.is_match(token)
+    if re.is_match(token) {
+        return Ok(());
+    }
+
+    Err("Введенная строка не может быть валидным токеном.")
+}
+
+/// Читает строку с токеном и валидирует
+fn read_token() -> String {
+    Input::new()
+        .with_prompt("Введите VK API access_token")
+        .validate_with(|token: &String| -> Result<(), &str> {
+            match token_first_validation(token) {
+                Ok(()) => Ok(()),
+                Err(message) => Err(message),
+            }
+        })
+        .interact()
+        .unwrap()
 }
 
 fn main() {
-    println!("Hello world!");
+    read_token();
 }
