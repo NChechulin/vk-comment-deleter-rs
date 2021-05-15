3ca800fdc6d4b00cf898d405287eb3bc2a580b17
Nikolay Chechulin <nchechulin@aol.com>
2021-05-15 19:44:12 +0300

    combine all the stuff together into a cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
 pub mod cli;
 pub mod comment_reading;
+pub mod vk_api;
+use std::thread;
+use std::time::Duration;
+use tokio;
 
-fn main() {
-    let path = String::from("/home/nikolay/Downloads/comments");
-    // let result = comment_reading::get_list_of_files(&path);
-    // for path in result {
-    //     println!("{}", path);
-    // }
-    let result = comment_reading::get_all_comments(&path);
-    println!("{}", result.len());
-    // let result = comment_reading::get_comments_from_file(&path);
-    // println!("{}", result.len());
-    // cli::read_token();
-    // println!("{}", cli::read_agreement());
+// Время в мс между запросами. Не рекомендуется уменьшать из-за ограничений VK API.
+const REQUEST_INTERVAL: u32 = 1400;
+const SLEEP_DURATION: Duration = Duration::from_millis(REQUEST_INTERVAL as u64);
+
+fn calculate_total_time(comments_num: u32) -> u32 {
+    let total_ms: f32 = (REQUEST_INTERVAL as f32) * (comments_num as f32);
+    (total_ms / 60_000.0) as u32
+}
+
+#[tokio::main]
+async fn main() {
+    let token = cli::read_token();
+    let path = cli::read_comments_dir();
+
+    if !cli::read_agreement() {
+        println!("Комментарии не будут удалены");
+        return;
+    }
+
+    println!("Начинаю загружать все комментарии...");
+    let all_comments = comment_reading::get_all_comments(&path);
+    println!("Комментарии загружены: всего {} шт.", all_comments.len());
+
+    println!(
+        "Начинаю удаление. Займет примерно {} мин.",
+        calculate_total_time(all_comments.len() as u32)
+    );
+
+    let mut client = reqwest::Client::new();
+
+    for (number, comment) in all_comments.iter().enumerate() {
+        match comment
+            .send_delete_comment_request(&token, &mut client)
+            .await
+        {
+            Ok(_) => {}
+            Err(e) => println!("Ошибка в комментарии #{} {}", number, e),
+        }
+        thread::sleep(SLEEP_DURATION);
+    }
 }
